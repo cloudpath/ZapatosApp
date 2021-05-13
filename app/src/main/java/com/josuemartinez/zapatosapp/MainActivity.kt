1aef9b7beeabfb835a194c9b792549c155e275f3
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2021-05-13 12:59:17 -0400

    Create destinations
    Create destinations Login and Welcome

--- app/src/main/java/com/josuemartinez/zapatosapp/MainActivity.kt
+++ app/src/main/java/com/josuemartinez/zapatosapp/MainActivity.kt
@@ -7,9 +7,32 @@ import timber.log.Timber
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_welcome)
         Timber.plant(Timber.DebugTree())
+
+
+
+//        topAppBar.setNavigationOnClickListener {
+//            // Handle navigation icon press
+//        }
+//
+//        topAppBar.setOnMenuItemClickListener { menuItem ->
+//            when (menuItem.itemId) {
+//                R.id.instructions -> {
+//                    // Handle favorite icon press
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+
+
+
     }
+
+
+
+
 }
 
 
