b3bdc5cf37bce9dcc156e648aba34b5711df99af
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2021-05-19 12:51:12 -0400

    Add Login Fragment Manager

--- app/src/main/java/com/josuemartinez/zapatosapp/MainActivity.kt
+++ app/src/main/java/com/josuemartinez/zapatosapp/MainActivity.kt
@@ -1,38 +1,34 @@
 package com.josuemartinez.zapatosapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import timber.log.Timber
 
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_login)
+        setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
 
-
-
-//        topAppBar.setNavigationOnClickListener {
-//            // Handle navigation icon press
-//        }
-//
-//        topAppBar.setOnMenuItemClickListener { menuItem ->
-//            when (menuItem.itemId) {
-//                R.id.instructions -> {
-//                    // Handle favorite icon press
-//                    true
-//                }
-//                else -> false
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.commit {
+//                setReorderingAllowed(true)
+//                add<LoginFragment>(R.id.fragment_login_view)
 //            }
 //        }
 
 
-
     }
 
-
-
-
 }
+
+
+
 
 
---
+++ app/src/main/java/com/josuemartinez/zapatosapp/User.kt
@@ -0,0 +1,5 @@
+package com.josuemartinez.zapatosapp
+
+import android.widget.Button
+
+data class User(var email: String = "", var password: String = "")
