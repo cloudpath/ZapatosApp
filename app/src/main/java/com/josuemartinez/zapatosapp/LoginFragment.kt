53e9d177b5fb388c364c98392b22605ce84807d2
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2021-05-20 12:02:30 -0400

    Add LoginFragment to Activity

--- app/src/main/java/com/josuemartinez/zapatosapp/LoginFragment.kt
+++ app/src/main/java/com/josuemartinez/zapatosapp/LoginFragment.kt
@@ -1,43 +1,25 @@
 package com.josuemartinez.zapatosapp
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.josuemartinez.zapatosapp.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
-    private lateinit var binding: FragmentLoginBinding
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val _binding get() = binding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false)
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentLoginBinding.inflate(inflater, container, false)
-
-        binding.login.setOnClickListener {
-            requireActivity().run {
-                startActivity(Intent(this, MainActivity::class.java))
-                finish() // If activity no more needed in back stack
-            }
-        }
         return binding.root
 
 
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
 
 }
