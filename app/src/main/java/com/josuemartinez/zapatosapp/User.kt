32fd04bfd5bac40d54b942a786763833cb589b20
Josue Martinez <46319076+cloudpath@users.noreply.github.com>
2021-05-23 22:48:27 -0400

    Add Listing Activity

--- app/src/main/java/com/josuemartinez/zapatosapp/User.kt
+++ app/src/main/java/com/josuemartinez/zapatosapp/User.kt
@@ -1,5 +1,3 @@
 package com.josuemartinez.zapatosapp
 
-import android.widget.Button
-
 data class User(var email: String = "", var password: String = "")
---
+++ app/src/main/java/com/josuemartinez/zapatosapp/models/ShoeViewModel.kt
@@ -0,0 +1,9 @@
+package com.josuemartinez.zapatosapp.models
+
+import androidx.lifecycle.ViewModel
+
+class ShoeViewModel : ViewModel() {
+    init {
+        
+    }
+}
