0497a5c4691f9bffdb8231fd73ab236d32f1820a
Josue Martinez <46319076+cloudpath@users.noreply.github.com>
2021-06-03 10:19:33 -0400

    Update FAB
    Update floating action button

--- app/src/main/java/com/josuemartinez/zapatosapp/ListingActivity.kt
+++ app/src/main/java/com/josuemartinez/zapatosapp/ListingActivity.kt
@@ -1,32 +1,29 @@
 package com.josuemartinez.zapatosapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import com.josuemartinez.zapatosapp.models.Shoe
-import com.josuemartinez.zapatosapp.models.ShoeViewModel
+import com.josuemartinez.zapatosapp.databinding.ActivityListingBinding
+import com.josuemartinez.zapatosapp.viewmodels.ShoeViewModel
 
 class ListingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityListingBinding
 
     private lateinit var viewModel: ShoeViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_listing)
+        // Inflate view and obtain an instance of the binding class.
+        val binding: ActivityListingBinding = DataBindingUtil.setContentView(this, R.layout.activity_listing)
 
-
-
-
-//        startActivity(
-//            Intent(this, ListingFragment::class.java).apply {
-//                putExtra("shoe1", shoe1)
-//            }
-//        )
-
+        // Specify the current activity as the lifecycle owner.
+        binding.lifecycleOwner = this
 
         viewModel = ViewModelProvider(this).get(ShoeViewModel::class.java)
 
 
     }
 }
+
