197bdfd7c304b24313d9db662ee35682f746262f
Josue Martinez <46319076+cloudpath@users.noreply.github.com>
2021-06-12 13:55:32 -0400

    Update ItemsList.kt

--- app/src/main/java/com/josuemartinez/zapatosapp/ItemsList.kt
+++ app/src/main/java/com/josuemartinez/zapatosapp/ItemsList.kt
@@ -24,6 +24,7 @@ class ItemsList: LinearLayout {
 
     fun filledShoe(shoe: Shoe) {
         bindingList.apply {
+            addView(this.root)
             textShoeName.text = shoe.name
             textShoeCompany.text = shoe.company
             textShoeSize.text = shoe.size.toString()
