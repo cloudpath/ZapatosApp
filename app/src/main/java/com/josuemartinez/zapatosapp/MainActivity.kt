311582e87e1f687d486e02d08d2e57db07aa212a
Josue Martinez <46319076+cloudpath@users.noreply.github.com>
2021-06-13 12:35:47 -0400

    Update MainActivity.kt
    Setup the nav controller and Toolbar

--- app/src/main/java/com/josuemartinez/zapatosapp/MainActivity.kt
+++ app/src/main/java/com/josuemartinez/zapatosapp/MainActivity.kt
@@ -1,17 +1,43 @@
 package com.josuemartinez.zapatosapp
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.josuemartinez.zapatosapp.databinding.ActivityMainBinding
 import timber.log.Timber
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+
+        setContentView(binding.root)
+
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
 
     }
 
